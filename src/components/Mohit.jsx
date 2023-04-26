ff4c6e25e5f8c6d9212d6fad12dbbb9ac0443ee1
radialcode-mohit <mohitgirdhar9034@gmail.com>
2023-04-26 09:38:53 +0530

    changes

---
+++ src/components/Mohit.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Mohit = () => {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis,
+      ullam! Deserunt fugiat laborum ipsa ducimus quia, illum illo distinctio,
+      minima expedita quod ad dicta inventore? Nesciunt ipsa non ab doloribus.
+    </div>
+  );
+};
+
+export default Mohit;
