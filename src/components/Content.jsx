239d676818f3caf17a91f9fa856c6d3e3e6053ea
Your Name <you@example.com>
2023-04-26 09:33:58 +0530

    first

---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='clr_black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem tempora laudantium placeat repudiandae rem mollitia quaerat dolor, at qui quo nam voluptatem. Mollitia, voluptatum obcaecati. Dolores iusto esse a quibusdam. Doloremque placeat laudantium minima rerum ut velit pariatur aliquid asperiores, eveniet, recusandae impedit minus rem? Nobis minima corrupti excepturi.</div>
+  )
+}
+
+export default Content
